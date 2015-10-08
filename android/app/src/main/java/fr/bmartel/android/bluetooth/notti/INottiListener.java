1e4d81836bf3aa423bfcab0c7b545980a5df1a09
Bertrand Martel <bmartel.fr@gmail.com>
2015-10-08 13:07:01 +0200

    fix Notti listener

---
+++ android/app/src/main/java/fr/bmartel/android/bluetooth/notti/INottiListener.java
@@ -0,0 +1,14 @@
+package fr.bmartel.android.bluetooth.notti;
+
+import java.util.ArrayList;
+
+
+/**
+ * Generic interface listener for Notti device
+ *
+ * @author Bertrand Martel
+ */
+public interface INottiListener {
+
+
+}
